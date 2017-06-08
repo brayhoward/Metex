0d379392b98bb47d7ef919a442b6545de84c1a60
brayhoward <brandonrhoward@gmail.com>
2017-06-08 15:46:34 -0500

    App for getting temperature in Fahrenheit for a city. Init commit

---
+++ .history/lib/worker_20170608152033.ex
@@ -0,0 +1,43 @@
+defmodule Metex.Worker do
+  require IEx
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+      :error ->
+        "Location #{location} not found"
+    end
+  end
+
+  def url_for location do
+    location = URI.encode(location)
+
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=12167dc2b6f421ee44367e4d20feeec8"
+  end
+
+  #
+  ## Parse response
+  #
+  defp parse_response({:ok, %HTTPoison.Response{ body: body, status_code: 200 }}) do
+
+    body
+    |> JSON.decode!
+    |> compute_temperature
+  end
+
+  defp parse_response(_), do: :error
+
+  defp compute_temperature(json) do
+    IEx.pry
+
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+
+    rescue
+      _ -> :error
+    end
+  end
+end
---
+++ .history/lib/worker_20170608154336.ex
@@ -0,0 +1,46 @@
+defmodule Metex.Worker do
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°F"
+      :error ->
+        "Location #{location} not found"
+    end
+  end
+
+  def url_for location do
+    location = URI.encode(location)
+
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=12167dc2b6f421ee44367e4d20feeec8"
+  end
+
+  #
+  ## Parse response
+  #
+  defp parse_response({:ok, %HTTPoison.Response{ body: body, status_code: 200 }}) do
+
+    body
+    |> JSON.decode!
+    |> compute_temperature
+  end
+
+  defp parse_response(_), do: :error
+
+  defp compute_temperature(json) do
+
+    try do
+      # Convert temp form Kelvin to Fahrenheit
+      k = json["main"]["temp"]
+
+      fahrenheit = (9/5 * (k - 273.15) + 32) |> Float.round(1)
+
+      {:ok, fahrenheit}
+
+    rescue
+      _ -> :error
+    end
+  end
+end
